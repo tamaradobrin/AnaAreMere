6a3e708da78ed447270fea7df9f672ba7fa95137
tamidobinda <tami.dobinda@gmail.com>
2017-05-19 17:59:21 +0300

    MTSP - Generate initial population WIP

--- src/main/java/com/mtsp/Solution.java
+++ src/main/java/com/mtsp/Solution.java
@@ -1,5 +1,9 @@
 package com.mtsp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Random;
 
 public class Solution {
@@ -22,13 +26,36 @@ public class Solution {
     this.instance = instance;
   }
 
-  public void generateSolution() {
+  public int[] generateSolution() {
     int m = instance.getM();
     int n = instance.getN();
-    int[][] coordinates = instance.getCoordinates();
+    chromosome = new int[n + m - 1];
+    Map<Integer, List<Integer>> initialSol = new HashMap<Integer, List<Integer>>();
+    for (int i = 0; i < m; i++) {
+      initialSol.put(i, new ArrayList<Integer>());
+    }
     Random r = new Random();
-    for (int i = 0; i < n; i++) {
-        int assignee = r.nextInt(m);
+    for (int i = 1; i < n; i++) {
+      int assignee = r.nextInt(m);
+      initialSol.get(assignee).add(i);
+    }
+    int k = 0;
+    for (int i = 0; i < m; i++) {
+      for (Integer city : initialSol.get(i)) {
+        chromosome[k++] = city;
+      }
+      chromosome[n + i - 1] = initialSol.get(i).size();
+    }
+    return chromosome;
+  }
+
+  public static void main(String[] args) {
+    MTSPInstance instance = new MTSPInstance(0, 2);
+    Solution s = new Solution();
+    s.setInstance(instance);
+    int[] sol = s.generateSolution();
+    for (int i = 0; i < instance.getN() + instance.getM(); i++) {
+      System.out.print(sol[i] + " ");
     }
   }
 }
